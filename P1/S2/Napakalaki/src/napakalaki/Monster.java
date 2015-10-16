235a9954df0cbedfc833f5b7ff37018f0d1ab6c5
juaneml <juanes.ml89@gmail.com>
2015-10-16 17:01:59 +0200

    Sesión 2

---
+++ P1/S2/Napakalaki/src/napakalaki/Monster.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author juane
+ * @version 1.1
+ * Clase Monster
+ */
+public class Monster {
+      
+    /* Atributos propios de la clase*/
+    
+    private String name; // nombre del monstruo
+    private int combatLevel; // nivel de combate del monstruo
+
+    /* Atributo de tipo Prize y BadConsequence */
+    private Prize price;
+    private BadConsequence bc;
+    
+    /* Constructor*/
+    
+    Monster(String name, int level, BadConsequence bc, Prize price){
+        
+        this.name = name;
+        this.combatLevel = level;
+        this.bc = bc;
+        this.price = price;
+    }
+    
+    /**
+     * Método getName()
+     * Devuelve el nombre del monstruo
+     * @return name de tipo int
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Método getCombatLevel()
+     * Devuelve el nivel de combate del monstruo
+     * @return 
+     */
+    public int getCombatLevel() {
+        return combatLevel;
+    }
+    
+    /**
+     * Método toString
+     * @return 
+     */
+    @Override
+    public String toString() {
+        return "Monster = " + name + ", combatLevel = " 
+                +  combatLevel + ", Price: " +  price 
+                + ", " + bc ;
+    }
+}
